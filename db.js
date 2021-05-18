15c637bb6e8fb60ceb05096edb3f8551862f5404
ArseniyXaoc <xaoc190789@gmail.com>
2021-05-18 17:18:55 +0300

    fix: add connection port

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
                                 //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
+                                const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
     host: 'localhost',
+    port: '5433',
     dialect: 'postgres'
 })
 
@@ -14,3 +15,5 @@ sequelize.authenticate().then(
         console.log(`Error: ${err}`);
     }
 )
+
+module.exports = sequelize;
