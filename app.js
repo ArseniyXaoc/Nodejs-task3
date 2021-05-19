e055b7abfb2267ca03e9689b8beee09bedb36be3
ArseniyXaoc <xaoc190789@gmail.com>
2021-05-19 14:52:19 +0300

    feat:fix require

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
-var game = require('./controllers/gamecontroller')
+var game = require('./controllers/gamecontroller');
 const PORT = 4000;
+const validateSession = require('./middleware/validate-session')
 
 app.use('/', (req, res, next) => {
     if (req.originalUrl === '/') {
@@ -14,9 +15,10 @@ app.use('/', (req, res, next) => {
 });
 
 db.sync();
-app.use(require('body-parser').json());
+
+app.use(express.json());
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
+app.use(validateSession);
 app.use('/api/game', game);
 app.listen(PORT, function() {
     console.log(`App is running on http://localhost:${PORT}`)
